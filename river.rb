0c3a8bed08f444f4f63207c053c343f22aa919a4
oliviadugger <oliviacdugger@gmail.com>
2018-07-13 19:04:38 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,9 @@
 # river.rb
+class River
+  def initialize(name, length, countries, discharge)
+    @name = name
+    @length = length
+    @countries = countries
+    @discharge = discharge
+  end
+end
