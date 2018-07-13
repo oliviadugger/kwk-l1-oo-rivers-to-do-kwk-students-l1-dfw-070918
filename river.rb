9222aec04104352bd07265b937a3a12038cd1e58
oliviadugger <oliviacdugger@gmail.com>
2018-07-13 19:15:32 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,20 @@
 # river.rb
+class River
+  attr_reader :name, :length, :countries, :discharge
+  attr_writer :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+ 
+  end
+  
+  def flood
+    @discharge *= 1.3
+  end
+  
+  def dry_up
+    @discharge *= 0.5
+    
+  end
+
+end
