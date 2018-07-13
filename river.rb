1994b4644266491d9208eff5be214163a0b8f38f
oliviadugger <oliviacdugger@gmail.com>
2018-07-13 19:06:48 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,9 +1,13 @@
 # river.rb
 class River
+  attr_reader :name, :length, :countries, :discharge
   def initialize(name, length, countries, discharge)
     @name = name
     @length = length
     @countries = countries
     @discharge = discharge
   end
+  
+  def flood
+    discharge 
 end
